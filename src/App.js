74c0a8b9cd21faa18717239228f3013b41514fa3
AkbarVali9 <shailakbarvali239@gmail.com>
2023-07-03 14:42:19 +0000

    BrowserHistory

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import History from './components/History'
+import './App.css'
+
+// These are the list used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <History />
+
+export default App
